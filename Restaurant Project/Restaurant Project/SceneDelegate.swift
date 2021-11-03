57a99253ad39cfca25d9cc09601cad2e8b67cd5d
Ling Hong <bohrsmodel@gmail.com>
2021-11-03 12:40:49 -0400

    finish tabbar

--- Restaurant Project/Restaurant Project/SceneDelegate.swift
+++ Restaurant Project/Restaurant Project/SceneDelegate.swift
@@ -17,26 +17,45 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else {
             return
         }
-        
         let window = UIWindow(windowScene: windowScene)
+        let tabBar = UITabBarController()
         let controller = UIStoryboard(name: "Main", bundle: .main).instantiateInitialViewController() as! ViewController
         let coordinator = Coordinator(controller)
+        let lunchImage = UIImage(named: "tab_lunch")
         controller.delegate = coordinator
-        let BottleRocketNC = UINavigationController(rootViewController: BottleRocketController())
-        let tabBar = UITabBarController()
-        coordinator.navigationController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
-        tabBar.viewControllers = [coordinator.navigationController, BottleRocketNC]
+        let tabBarBackgroundImage = UIImage(named: "cellGradientBackground")
+        coordinator.navigationController.tabBarItem = UITabBarItem(title: "Lunch", image: lunchImage, tag: 0)
+        tabBar.viewControllers = [coordinator.navigationController, createBottleRocketNC()]
+        tabBar.tabBar.backgroundImage = tabBarBackgroundImage
+        tabBar.tabBar.tintColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1.0
+        )
+        
+        tabBar.tabBar.isTranslucent = false
+        //tabBar.tabBar.isOpaque = false
         window.rootViewController = tabBar
         // window.rootViewController = coordinator.navigationController
         self.window = window
         window.makeKeyAndVisible()
         
     }
+
+//func createCoordinatorNC()->UINavigationController{
+//    let controller = UIStoryboard(name: "Main", bundle: .main).instantiateInitialViewController() as! ViewController
+//    let coordinator = Coordinator(controller)
+//    controller.delegate = coordinator
+//    coordinator.navigationController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
+//    return UINavigationController(rootViewController: coordinator.navigationController  )
+//  
+//}
 }
 func createBottleRocketNC()->UINavigationController{
     let BottleRockeVC = BottleRocketController()
     BottleRockeVC.title = "Internet"
-    BottleRockeVC.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+    let internetImage = UIImage (named: "tab_internets")
+    
+    let tabBarItem = UITabBarItem(title: "Internet", image: internetImage, tag: 1)
+    BottleRockeVC.tabBarItem = tabBarItem
+    
     return UINavigationController(rootViewController: BottleRockeVC)
 }
 
